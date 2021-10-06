288e0e037741c48b8e0708f344c65064db278e11
Giulia Chiola <giulia.chiola.17@gmail.com>
2021-10-06 12:43:56 +0200

    style: cleanup ESlint warnings

--- src/API.js
+++ src/API.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 import {
   SEARCH_BASE_URL,
   POPULAR_BASE_URL,
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // Routing
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
 // Components
 import Header from './components/Header'
 // Pages
